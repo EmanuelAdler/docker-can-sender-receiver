a4bdc724accdaa97ae3bba040567427381056a17
p-r-m-n <paulo_rmn2010@hotmail.com>
2025-03-04 12:20:58 -0300

    Criação da estrutura de testes unitários

---
+++ tests/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// descomentar quando os arquivos em src forem modularizados
+//#include "../src/sender.h"  
+//#include "../src/receiver.h"
+
+int main(){
+
+
+
+    return 0;
+}
