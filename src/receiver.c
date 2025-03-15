6982742f42927802e14583678f1e4f2a1b8e8089
p-r-m-n <paulo_rmn2010@hotmail.com>
2025-03-15 18:07:22 -0300

    update receiver source code to be MISRA-C compliant

--- src/receiver.c
+++ src/receiver.c
@@ -1,19 +1,47 @@
 #include "can_socket.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-    int sock = create_can_socket("vcan0");
-    if (sock < 0) return 1;
+#define CAN_INTERFACE      ("vcan0")
+#define SUCCESS_CODE       (0)
+#define ERROR_CODE         (1)
+#define RECEIVE_SUCCESS    (1)
 
+int main(void)
+{
+    int sock = -1;
     struct can_frame frame;
+    int32_t receive_result;
+    
+    sock = create_can_socket(CAN_INTERFACE);
+    if (sock < 0)
+    {
+        return ERROR_CODE;
+    }
 
-    // Receive CAN frame
-    while (1) {
-        if (receive_can_frame(sock, &frame) > 0) {
-            printf("Received CAN ID: %X Data: %s\n", frame.can_id, frame.data);
-            fflush(stdout);
+    (void)memset(&frame, 0, sizeof(frame));
+
+    for(;;)
+    {
+        receive_result = receive_can_frame(sock, &frame);
+        if (receive_result == RECEIVE_SUCCESS)
+        {
+            (void)printf("Received CAN ID: %04X Data: ", (unsigned int)frame.can_id);
+            for(uint8_t i = 0U; i < frame.can_dlc; i++)
+            {
+                (void)printf("%02X ", frame.data[i]);
+            }
+            (void)printf("\n");
+            (void)fflush(stdout);
+        }
+        else if (receive_result < 0)
+        {
+            break;
         }
     }
 
-    close_can_socket(sock);
-    return 0;
+    (void)close_can_socket(sock);
+    return SUCCESS_CODE;
 }
